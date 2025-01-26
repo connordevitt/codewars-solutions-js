cd8ac10d3d59c351a5016c1f628e1101ea5a0442
Connor  Devitt <84610739+connordevitt@users.noreply.github.com>
2025-01-26 13:29:28 -0500

    Update digitize_reversed_array.js

--- Easy/digitize_reversed_array.js
+++ Easy/digitize_reversed_array.js
@@ -3,15 +3,14 @@
 // Link: https://www.codewars.com/kata/convert-number-to-reversed-array-of-digits
 
 function digitize(n) {
-    let initialArray = ("" + n).split('');
-    let reversedArray = [];
-    for (var i = initialArray.length - 1; i >= 0; i--) {
-      reversedArray[i] = parseInt(initialArray.shift(), 10);
-    }
-    return reversedArray;
+  let initialArray = (""+n).split('');
+  let reversedArray = [];
+  for (var i = initialArray.length - 1; i >= 0; i--) {
+    reversedArray[i] = parseInt(initialArray.shift(),10);
   }
-  
-  // Test cases
-  console.log(digitize(348597)); // [7, 9, 5, 8, 4, 3]
-  console.log(digitize(12345));  // [5, 4, 3, 2, 1]
+  return reversedArray;
+}
+
+// Test cases:
+console.log(digitize(348597));
   
