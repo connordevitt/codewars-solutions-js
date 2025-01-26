aaf5ac74b860bf94dc2d2d0a32964e47ded4a816
Connor  Devitt <84610739+connordevitt@users.noreply.github.com>
2025-01-26 13:30:13 -0500

    Update reversed_strings.js

--- Easy/reversed_strings.js
+++ Easy/reversed_strings.js
@@ -2,14 +2,10 @@
 // Description: Reverse the provided string.
 // Link: https://www.codewars.com/kata/reversed-strings
 
-function solution(str) {
-    const splitString = str.split('');
-    const reverseArray = splitString.reverse();
-    const joinArray = reverseArray.join("");
-    return joinArray;
-  }
-  
-  // Test cases
-  console.log(solution("hello")); // "olleh"
-  console.log(solution("world")); // "dlrow"
+function solution(str){
+  const splitString = str.split('');
+  const reverseArray = splitString.reverse();
+  const joinArray = reverseArray.join("")
+  return joinArray;
+}
   
