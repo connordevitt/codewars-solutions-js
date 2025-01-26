067d13f07becf0eedddecb7fc2fe8c6c705db660
Connor  Devitt <84610739+connordevitt@users.noreply.github.com>
2025-01-26 13:29:00 -0500

    Update are_you_playing_banjo.js

--- Easy/are_you_playing_banjo.js
+++ Easy/are_you_playing_banjo.js
@@ -3,16 +3,17 @@
 // Link: https://www.codewars.com/kata/are-you-playing-banjo
 
 function areYouPlayingBanjo(name) {
-    if (name[0] === 'R' || name[0] === 'r') {
-      return name + " plays banjo";
-    } else {
-      return name + " does not play banjo";
-    }
+  // Implement me
+  if (name[0] === 'R' || name[0] === 'r') {
+    return name + " plays banjo";
+  } else {
+    return name + " does not play banjo";
   }
-  
-  // Test cases
-  console.log(areYouPlayingBanjo('Richard')); // "Richard plays banjo"
-  console.log(areYouPlayingBanjo('Ringo'));   // "Ringo plays banjo"
-  console.log(areYouPlayingBanjo('royce'));   // "royce plays banjo"
-  console.log(areYouPlayingBanjo('Alice'));   // "Alice does not play banjo"
+}
+
+// Test cases:
+
+console.log( areYouPlayingBanjo('Richard'));
+console.log( areYouPlayingBanjo('Ringo'));
+console.log( areYouPlayingBanjo('royce'));
   
